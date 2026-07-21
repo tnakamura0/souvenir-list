f5fa9bb447c6f5148700d36a986ad2ced87f9c61
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2026-07-21 11:10:48 +0900

    feat: ログイン状態を判定する仕組みを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,28 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  helper_method :logged_in?, :current_user
+  before_action :require_login
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  def logout
+    session[:user_id] = nil
+    @current_user = nil
+  end
+
+  def require_login
+    redirect_to login_path unless logged_in?
+  end
 end
