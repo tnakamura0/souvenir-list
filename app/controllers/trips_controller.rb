3569ddd49fda87bd9fd0379e2fa15ce671f94c00
tnakamura0 <206074491+tnakamura0@users.noreply.github.com>
2026-07-24 21:57:47 +0900

    feat: 旅行作成機能を実装 (#76)
    * feat: 旅行作成機能を実装
    
    * test: 旅行作成機能のリクエストスペックを作成
    
    * feat: 戻るボタンをインライン要素に変更

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,4 +2,24 @@ class TripsController < ApplicationController
   def index
     @trips = current_user.trips
   end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = current_user.trips.build(trip_params)
+    if @trip.save
+      redirect_to trips_path, notice: t(".success")
+    else
+      flash.now[:alert] = t(".failure")
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:name, :destination, :departure_date, :return_date)
+  end
 end
