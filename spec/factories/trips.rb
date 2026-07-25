b42011d3401a85125e7d46659465a6ad3263ad99
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2026-07-25 13:53:40 +0900

    refactor: 旅行を作成するリクエストスペックのリファクタリング

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     association :user
     name { "名古屋旅行" }
     destination { "名古屋、岐阜" }
-    departure_date { "2026-07-23" }
-    return_date { "2026-07-25" }
+    departure_date { Date.new(2026, 7, 23) }
+    return_date { Date.new(2026, 7, 25) }
   end
 end
