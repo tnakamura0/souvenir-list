759405082d312e9a093f490e209e33d7253b2022
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2026-07-28 09:44:44 +0900

    test: 相手登録（作成）機能のリクエストスペックを実装

--- spec/requests/recipients_spec.rb
+++ spec/requests/recipients_spec.rb
@@ -35,4 +35,100 @@ RSpec.describe "Recipients", type: :request do
       end
     end
   end
+
+  describe "GET /recipients/new" do
+    context "ログインしている場合" do
+      let(:user) { create(:user) }
+
+      before do
+        login_as(user)
+      end
+
+      it "相手登録画面を表示する" do
+        get new_recipient_path
+
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "ログインしていない場合" do
+      it "ログイン画面へリダイレクトする" do
+        get new_recipient_path
+
+        expect(response).to redirect_to(login_path)
+      end
+    end
+  end
+
+  describe "POST /recipients" do
+    context "ログインしている場合" do
+      let(:user) { create(:user) }
+
+      before do
+        login_as(user)
+      end
+
+      context "正常な値の場合" do
+        let(:valid_params) do
+          {
+            recipient: attributes_for(:recipient)
+          }
+        end
+
+        it "相手を作成する" do
+          expect {
+            post recipients_path, params: valid_params
+          }.to change(Recipient, :count).by(1)
+        end
+
+        it "作成した相手がログインユーザーと紐づく" do
+          post recipients_path, params: valid_params
+
+          expect(Recipient.last.user).to eq(user)
+        end
+
+        it "相手一覧画面へリダイレクトする" do
+          post recipients_path, params: valid_params
+
+          expect(response).to redirect_to(recipients_path)
+        end
+      end
+
+      context "不正な値の場合" do
+        let(:invalid_params) do
+          {
+            recipient: attributes_for(:recipient, name: nil)
+          }
+        end
+
+        it "相手を作成しない" do
+          expect {
+            post recipients_path, params: invalid_params
+          }.not_to change(Recipient, :count)
+        end
+
+        it "相手登録画面を再表示する" do
+          post recipients_path, params: invalid_params
+
+          expect(response).to have_http_status(:unprocessable_content)
+        end
+      end
+    end
+
+    context "ログインしていない場合" do
+      let(:valid_params) do
+        {
+          recipient: attributes_for(:recipient)
+        }
+      end
+
+      it "相手を作成せずにログイン画面へリダイレクトする" do
+        expect {
+          post recipients_path, params: valid_params
+        }.not_to change(Recipient, :count)
+
+        expect(response).to redirect_to(login_path)
+      end
+    end
+  end
 end
