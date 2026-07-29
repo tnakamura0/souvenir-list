7112bc364fd953ec97a66f5ccecd3857cdfd9cf7
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2026-07-29 10:20:42 +0900

    test: TripRecipientのモデルスペックを作成

---
+++ spec/models/trip_recipient_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe TripRecipient, type: :model do
+  let(:user) { create(:user) }
+  let(:trip) { create(:trip, user:) }
+  let(:recipient) { create(:recipient, user:) }
+
+  describe "バリデーション" do
+    subject(:trip_recipient) do
+      build(:trip_recipient, trip:, recipient:)
+    end
+
+    context "すべての属性が有効な場合" do
+      it "有効である" do
+        expect(trip_recipient).to be_valid
+      end
+    end
+
+    context "同じ旅行に同じ相手が既に追加されている場合" do
+      before do
+        create(:trip_recipient, trip:, recipient:)
+      end
+
+      it "無効である" do
+        expect(trip_recipient).to be_invalid
+        expect(trip_recipient.errors[:recipient_id]).to be_present
+      end
+    end
+  end
+end
