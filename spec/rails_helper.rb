e03b389e59850fd7d37c0d187896ea26fb504901
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2026-07-30 14:42:56 +0900

    chore: gem "bullet"を導入

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,4 +71,13 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryBot::Syntax::Methods
+
+  config.before(:each) do
+    Bullet.start_request
+  end
+
+  config.after(:each) do
+    Bullet.perform_out_of_channel_notifications if Bullet.notification?
+    Bullet.end_request
+  end
 end
