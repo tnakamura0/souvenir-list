e2525f6ac77ac3254d80ef5265c4e7373e91398d
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2026-08-04 14:52:34 +0900

    feat: タグ作成機能を実装

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,4 +3,22 @@ class TagsController < ApplicationController
     @tags = current_user.tags
     @tag = Tag.new
   end
+
+  def create
+    @tag = current_user.tags.build(tag_params)
+
+    if @tag.save
+      redirect_to tags_path, notice: t(".success")
+    else
+      @tags = current_user.tags.where.not(id: nil)
+      flash.now[:alert] = t(".failure")
+      render :index, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
 end
--- spec/requests/tags_spec.rb
+++ spec/requests/tags_spec.rb
@@ -43,4 +43,92 @@ RSpec.describe "Tags", type: :request do
       end
     end
   end
+
+  describe "POST /tags" do
+    context "ログインしている場合" do
+      let(:user) { create(:user) }
+
+      before do
+        login_as(user)
+      end
+
+      context "正常な値の場合" do
+        let(:valid_params) do
+          {
+            tag: attributes_for(:tag)
+          }
+        end
+
+        it "タグを作成する" do
+          expect {
+            post tags_path, params: valid_params
+          }.to change(Tag, :count).by(1)
+        end
+
+        it "作成したタグがログインユーザーと紐づく" do
+          post tags_path, params: valid_params
+
+          expect(Tag.last.user).to eq(user)
+        end
+
+        it "タグ一覧画面へリダイレクトする" do
+          post tags_path, params: valid_params
+
+          expect(response).to redirect_to(tags_path)
+        end
+
+        it "成功時のフラッシュメッセージを設定する" do
+          post tags_path, params: valid_params
+
+          expect(flash[:notice]).to eq("タグを作成しました")
+        end
+      end
+
+      context "不正な値の場合" do
+        let(:invalid_params) do
+          {
+            tag: attributes_for(:tag, name: "")
+          }
+        end
+
+        it "タグを作成しない" do
+          expect {
+            post tags_path, params: invalid_params
+          }.not_to change(Tag, :count)
+        end
+
+        it "タグ一覧画面を再表示する" do
+          post tags_path, params: invalid_params
+
+          expect(response).to have_http_status(:unprocessable_content)
+        end
+
+        it "失敗時のフラッシュメッセージを設定する" do
+          post tags_path, params: invalid_params
+
+          expect(flash[:alert]).to eq("タグを作成できませんでした")
+        end
+      end
+    end
+
+    context "ログインしていない場合" do
+      let(:valid_params) do
+        {
+          tag: attributes_for(:tag)
+        }
+      end
+
+      it "タグを作成しない" do
+        expect {
+          post tags_path, params: valid_params
+        }.not_to change(Tag, :count)
+      end
+
+      it "ログイン画面へリダイレクトする" do
+        post tags_path, params: valid_params
+
+        expect(response).to redirect_to(login_path)
+      end
+    end
+  end
 end
