2114f85d8184a86b78115e13dfbf8ebf6a7d2b8b
tnakamura0 <206074491+tnakamura0@users.noreply.github.com>
2026-08-10 16:00:19 +0900

    style: UIパーツのデザインを調整 (#127)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def navigation_active?(section)
+    case section
+    when :today
+      controller_name == "today"
+    when :trips
+      controller_name.in?(%w[trips trip_recipients])
+    when :recipients
+      controller_name.in?(%w[recipients tags])
+    end
+  end
 end
