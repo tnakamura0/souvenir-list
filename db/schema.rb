e57c6a7f4fbb01ccc338f89267f88325b94aaca6
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2026-08-17 15:41:29 +0900

    feat: trip_recipientsテーブルにsouvenir_nameカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_31_114504) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_17_063901) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_31_114504) do
     t.datetime "created_at", null: false
     t.boolean "purchased", default: false, null: false
     t.bigint "recipient_id", null: false
+    t.string "souvenir_name"
     t.bigint "trip_id", null: false
     t.datetime "updated_at", null: false
     t.index ["recipient_id"], name: "index_trip_recipients_on_recipient_id"
