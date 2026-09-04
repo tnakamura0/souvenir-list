1598a155efb0055fd5ae2867203901fed7f598ac
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2026-09-04 15:13:57 +0900

    feat: 複数のdialogを扱えるように拡張

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,17 +3,32 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["dialog"]
 
-  open() {
+  open(event) {
+    const name = event.params.dialog
+
+    if (name) {
+      const dialog = this.dialogTargets.find((dialog) => dialog.dataset.modalName === name)
+      dialog?.showModal()
+      return
+    }
+
     this.dialogTarget.showModal()
   }
 
-  close() {
+  close(event) {
+    const dialog = event.currentTarget.closest("dialog")
+
+    if (dialog) {
+      dialog.close()
+      return
+    }
+
     this.dialogTarget.close()
   }
 
   closeOnBackdrop(event) {
-    if (event.target === this.dialogTarget) {
-      this.dialogTarget.close()
+    if (event.target === event.currentTarget) {
+      event.currentTarget.close()
     }
   }
 }
